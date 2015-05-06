204239ccdfe7740d4bebad8fd5ac81f1772770f1
z5019266 <z5019266@cse.unsw.EDU.AU>
2015-05-06 18:57:30 +1000

    Lots of vertical/horizontal mindfuckery...lots of things are broken

--- Board.java
+++ Board.java
@@ -4,49 +4,92 @@ import java.util.ArrayList;
 public class Board {
 	//Fields
 	private ArrayList<ArrayList<Spot>> board;
+	private int columns;
+	private int rows;
 	
 	//Constructor
 	public Board(){
-      //Columns containing rows
+		//Columns containing rows (Go across the top row of 7 and generate down 6 rows of 1
+		//Spots index start from the 'bottom' of the game board
 		int counter = 0;
 		int counterTwo = 0;
 		ArrayList<ArrayList<Spot>> Columns = new ArrayList<ArrayList<Spot>>();
-		while(counterTwo < 6){
+		System.out.println("Generating Board...");
+		while(counterTwo < 7){
 			ArrayList<Spot> Row = new ArrayList<Spot>();
-			while(counter < 7){
+			while(counter < 6){
+				System.out.print("Blank ");
 				Spot s = new Spot();
 				Row.add(s);
 				counter ++;
 			}
+			System.out.println();
+			counter = 0;
 			Columns.add(Row);
 			counterTwo ++;
 		}
 		this.board = Columns;
+		this.columns = Columns.size();
+		this.rows = Columns.get(0).size();
 	}
 	//Methods
 	public ArrayList<ArrayList<Spot>> getBoard(){
 		return this.board;
 	}
+	public int getColumns(){
+		return this.columns;
+	}
+	public int getRows(){
+		return this.rows;
+	}
+	public void printBoard(){
+		System.out.println("Printing board...");
+		int row = 0;
+		int column = 0;
+		while(column < this.columns){
+			while(row < this.rows){
+			System.out.print(this.board.get(column).get(row).getState() + " ");
+			row ++;
+			}
+			row = 0;
+			column ++;
+			System.out.println();
+		}
+		System.out.println();
+	}
+	public boolean addPiece(Player p, int column){
+		boolean addSuccess = false;
+		ArrayList<Spot> columnToAdd = this.board.get(column);
+		int index = 0;
+		while(!(columnToAdd.get(index).getState().equals("Blank")) && index < 6){
+			index ++;
+		}
+		if(columnToAdd.get(index).getState().equals("Blank")){
+			addSuccess = true;
+			columnToAdd.get(index).changeState(p.getColor());
+		}
+		return addSuccess;
+	}
 	public boolean hasConnectFour(Player p){
 		boolean hasConnectFour = false;
 		if(!hasConnectFour){
-			hasConnectFour = hasHoriConnectFour(p);
+			hasConnectFour = hasVertConnectFour(p);
 		}
 		if(!hasConnectFour){
-			hasConnectFour = hasVertConnectFour(p);
+			hasConnectFour = hasHoriConnectFour(p);
 		}
 		if(!hasConnectFour){
 			hasConnectFour = hasDiagConnectFour(p);
 		}
 		return hasConnectFour;
 	}
-	public boolean hasHoriConnectFour(Player p){
+	public boolean hasVertConnectFour(Player p){
 		boolean hasConnectFour = false;
 		String type = p.getColor();
 		for(ArrayList<Spot> a : this.board){
 			int index = 0;
 			int connected = 0;
-			while(index < 7){
+			while(index < getRows()){
 				if(a.get(index).getState().equals(type)){
 					connected ++;
 					if(connected >= 4){
@@ -59,15 +102,21 @@ public class Board {
 				}
 				index ++;
 			}
+			if(hasConnectFour){
+				break;
+			}
+		}
+		if(hasConnectFour){
+			System.out.print("vertical win");
 		}
 		return hasConnectFour;
 	}
-	public boolean hasVertConnectFour(Player p){
+	public boolean hasHoriConnectFour(Player p){
 		boolean hasConnectFour = false;
 		String type = p.getColor();
 		int index = 0;
 		int connected = 0;
-		while(index < 6){
+		while(index < this.getRows()){
 			for(ArrayList<Spot> a : this.board){
 				if(a.get(index).getState().equals(type)){
 					connected ++;
@@ -78,6 +127,9 @@ public class Board {
 				}
 			}
 			index ++;
+		}
+		if(hasConnectFour){
+			System.out.println("Hi");
 		}
 		return hasConnectFour;
 	}
--- Game.java
+++ Game.java
@@ -1,11 +1,51 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Game {
 	//Fields
 	private Board gameBoard;
+	private ArrayList<Player> players;
 	//Constructor
 	public Game(){
 		this.gameBoard = new Board();
+		Player red = new Player("red");
+		Player yellow = new Player("yellow");
+		this.players = new ArrayList<Player>();
+		this.players.add(red);
+		this.players.add(yellow);
 	}
 	//Method
-	
+	public static void main(String args[]){
+		Game newGame = new Game();
+		Player winner = newGame.runGame();
+		System.out.println(winner.getColor() + " wins!");
+	}
+	public Player runGame(){
+		Scanner scanner = new Scanner(System.in);
+		int player = 0;
+		Player currentPlayer = this.players.get(player);
+		while(true){
+
+			this.gameBoard.printBoard();
+			System.out.println("Please enter the column you want to put a piece in.");
+
+			int column = scanner.nextInt() - 1;
+			boolean successMove = gameBoard.addPiece(currentPlayer, this.gameBoard.getColumns() - column - 1);
+			if(successMove){
+				if(player == 0){
+					player ++;
+				}
+				else{
+					player --;
+				}
+				if(this.gameBoard.hasConnectFour(currentPlayer)){
+					scanner.close();
+					break;
+				}
+			}
+			currentPlayer = this.players.get(player);
+		}
+		return currentPlayer;
+	}
 }
